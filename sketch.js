b706f8fad5b1372338b961b4be210ea6a0a07e97
DannyBoy1320 <160650417+DannyBoy1320@users.noreply.github.com>
2024-03-07 09:07:51 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(400);
+}
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(200);
+  strokeWeight(20);
+  point(200,200);
+  line(200, 0, 200, 400);
+  line(0, 200, 400, 200);
+  line(100, 0, 100, 400);
+  line(300, 0, 300, 400);
+  line(0, 100, 400, 100);
+  line(0, 300, 400, 300);
+  triangle(0, 0, 0, 200, 200, 200);
+  rect(200, 0, 200, 200);
+  ellipse(200, 300, 200);
+  noStroke();
+  fill('red');
+  random([min],[max])
+  print(random());
+  print(random(10));
+  print(random(90,100));
+} 
