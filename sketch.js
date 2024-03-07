3c9fcc6d1bdbae9494103a76955215070bf0fb54
DannyBoy1320 <160650417+DannyBoy1320@users.noreply.github.com>
2024-03-07 11:19:57 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,29 +3,20 @@ function setup() {
 }
 
 function draw() {
-  background(400);
-}
-function setup() {
-  createCanvas(500, 500);
-}
-
-function draw() {
   background(200);
-  strokeWeight(20);
-  point(200,200);
-  line(200, 0, 200, 400);
-  line(0, 200, 400, 200);
-  line(100, 0, 100, 400);
-  line(300, 0, 300, 400);
-  line(0, 100, 400, 100);
-  line(0, 300, 400, 300);
-  triangle(0, 0, 0, 200, 200, 200);
-  rect(200, 0, 200, 200);
-  ellipse(200, 300, 200);
-  noStroke();
-  fill('red');
-  random([min],[max])
-  print(random());
-  print(random(10));
-  print(random(90,100));
-} 
+  //face
+   fill("gold")
+  ellipse (200,200,300,300)
+   
+  //eyes
+  fill("black")
+  strokeWeight(5)
+  ellipse (150,150,20,20)
+  ellipse (250,150,20,20)
+  
+  //mouth
+  fill("Black")
+  strokeWeight(10)
+  ellipse (200,250,100,0)
+  
+}
